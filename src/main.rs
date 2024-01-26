d623af20f6cd40801b76a967e7db9cbb8a5dc20a
Tommi Mäklin <tommi@maklin.fi>
2024-01-26 17:38:21 +0200

    Pass parameters to panaani dereplicate via CLI.

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ struct Cli {
 enum Commands {
     // Print testing stuff
     Dereplicate {
+        // Input files
         #[arg(group = "input", required = true)]
         seq_files: Vec<String>,
 
@@ -40,11 +41,62 @@ enum Commands {
         temp_dir_path: Option<String>,
 
         // Dereplicate parameters
+        #[arg(short = 'b', long = "batch-step", default_value_t = 50)]
+        batch_step: usize,
+
+        // Clustering parameters
         #[arg(long = "ani-threshold", default_value_t = 0.97)]
         ani_threshold: f32,
 
-        #[arg(short = 'b', long = "batch-step", default_value_t = 50)]
-        batch_step: usize,
+        #[arg(long = "linkage-method", required = false)]
+        linkage_method: Option<String>,
+
+        // ANI estimation parameters
+        #[arg(long = "skani-kmer-size", default_value_t = 15)]
+        skani_kmer_size: u8,
+
+        #[arg(long = "kmer-subsampling-rate", default_value_t = 30)]
+        kmer_subsampling_rate: u16,
+
+        #[arg(long = "marker-compression-factor", default_value_t = 1000)]
+        marker_compression_factor: u16,
+
+        #[arg(long = "rescue-small", default_value_t = false)]
+        rescue_small: bool,
+
+        #[arg(long = "clip-tails", default_value_t = false)]
+        clip_tails: bool,
+
+        #[arg(long = "median", default_value_t = false)]
+        median: bool,
+
+        #[arg(long = "adjust-ani", default_value_t = false)]
+        adjust_ani: bool,
+
+        #[arg(long = "min-af", default_value_t = 0.)]
+        min_aligned_frac: f64,
+
+        // de Bruijn graph construction parameters
+        #[arg(long = "ggcat-kmer-size", default_value_t = 51)]
+        ggcat_kmer_size: u32,
+
+        #[arg(long = "min-kmer-count", default_value_t = 1)]
+        kmer_min_multiplicity: u64,
+
+        #[arg(long = "minimzer-length", required = false)]
+        minimizer_length: Option<usize>,
+
+        #[arg(long = "no-rc", default_value_t = false)]
+        no_reverse_complement: bool,
+
+        #[arg(long = "unitig-type", required = false)]
+        unitig_type: Option<String>,
+
+        #[arg(long = "prefer-memory", default_value_t = true)]
+        prefer_memory: bool,
+
+        #[arg(long = "intermediate-compression", required = false)]
+        intermediate_compression_level: Option<u32>,
     },
 
     Dist {
@@ -70,11 +122,27 @@ fn main() {
         // Run the full pipeline
         Some(Commands::Dereplicate {
             seq_files,
+            batch_step,
+            linkage_method,
+            skani_kmer_size,
+            kmer_subsampling_rate,
+            marker_compression_factor,
+            rescue_small,
+            clip_tails,
+            median,
+            adjust_ani,
+            min_aligned_frac,
+            ggcat_kmer_size,
+            kmer_min_multiplicity,
+            minimizer_length,
+            no_reverse_complement,
+            unitig_type,
+            prefer_memory,
+            intermediate_compression_level,
             threads,
             memory,
             temp_dir_path,
             ani_threshold,
-            batch_step,
         }) => {
             rayon::ThreadPoolBuilder::new()
                 .num_threads(*threads as usize)
@@ -84,24 +152,78 @@ fn main() {
 
             let params: panaani::PanaaniParams = panaani::PanaaniParams {
                 batch_step: *batch_step,
-                ani_threshold: *ani_threshold,
             };
+
+            let skani_params: panaani::dist::SkaniParams = panaani::dist::SkaniParams {
+                kmer_size: *skani_kmer_size,
+                kmer_subsampling_rate: *kmer_subsampling_rate,
+                marker_compression_factor: *marker_compression_factor,
+                rescue_small: *rescue_small,
+
+                clip_tails: *clip_tails,
+                median: *median,
+                adjust_ani: *adjust_ani,
+
+                min_aligned_frac: *min_aligned_frac,
+                ..Default::default()
+            };
+
+            let kodama_params = panaani::clust::KodamaParams {
+                cutoff: *ani_threshold,
+                method: if linkage_method.is_some() {
+                    match linkage_method.as_ref().unwrap().as_str() {
+                        "single" => kodama::Method::Single,
+                        "complete" => kodama::Method::Complete,
+                        "average" => kodama::Method::Average,
+                        "weighted" => kodama::Method::Weighted,
+                        "ward" => kodama::Method::Ward,
+                        "centroid" => kodama::Method::Centroid,
+                        "median" => kodama::Method::Median,
+                        &_ => kodama::Method::Single,
+                    }
+                } else {
+                    kodama::Method::Single
+                },
+                ..Default::default()
+            };
+
             let ggcat_params: panaani::build::GGCATParams = panaani::build::GGCATParams {
+                kmer_size: *ggcat_kmer_size,
+                kmer_min_multiplicity: *kmer_min_multiplicity,
+                minimizer_length: if minimizer_length.is_some() {
+                    *minimizer_length
+                } else {
+                    None
+                },
+                no_reverse_complement: *no_reverse_complement,
+                unitig_type: if unitig_type.is_some() {
+                    match unitig_type.as_ref().unwrap().as_str() {
+                        "greedymatchtigs" => ggcat_api::ExtraElaboration::GreedyMatchtigs,
+                        "unitiglinks" => ggcat_api::ExtraElaboration::UnitigLinks,
+                        "eulertigs" => ggcat_api::ExtraElaboration::Eulertigs,
+                        "pathtigs" => ggcat_api::ExtraElaboration::Pathtigs,
+                        &_ => ggcat_api::ExtraElaboration::GreedyMatchtigs,
+                    }
+                } else {
+                    ggcat_api::ExtraElaboration::GreedyMatchtigs
+                },
+                prefer_memory: *prefer_memory,
+                intermediate_compression_level: if intermediate_compression_level.is_some() {
+                    *intermediate_compression_level
+                } else {
+                    None
+                },
                 temp_dir_path: temp_dir_path.clone().unwrap_or("./".to_string()),
                 threads: *threads,
                 memory: *memory,
                 ..Default::default()
-            };
-            let kodama_params = panaani::clust::KodamaParams {
-                cutoff: *ani_threshold,
-                method: kodama::Method::Single,
             };
 
             let clusters = panaani::dereplicate(
                 &seq_files,
                 &seq_files,
                 Some(params),
-                None,
+                Some(skani_params),
                 Some(kodama_params),
                 Some(ggcat_params),
             );
